234abcc69cd048a4557fdce55620477ed9dc87ef
Elyud <elyudmichael@gmail.com>
2023-09-19 22:49:01 +0000

    reverted back

--- main.h
+++ main.h
@@ -4,13 +4,8 @@
 #include <stdarg.h>
 #include <unistd.h>
 #include <stddef.h>
-#include <stdio.h>
 
 int _printf(const char *format, ...);
-void print_unsigned_integer(unsigned int n);
-void print_integer(int n);
-void print_binary(int num);
-void print_octal(unsigned int num);
-void print_hexa(int unsigned num ,int flag);
+
 
 #endif
