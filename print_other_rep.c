965c243b0e7788adcea780b3bd5adbf00a30ca55
Elyud <elyudmichael@gmail.com>
2023-09-20 01:54:31 +0000

    betty

--- print_other_rep.c
+++ print_other_rep.c
@@ -8,20 +8,21 @@
 */
 void print_binary(va_list list)
 {
-    int b[64];
-    int i, num;
-
-    num = va_arg(list, unsigned int);
-    i = 0;
-    while (num > 0)
-    {
-        b[i] = (num % 2) + 48;
-        num /= 2;
-        i++;
-        }
-    for (int j = i - 1; j >= 0; j--) {
-        write(1, &(b[j]), 1);
-    }
+	int b[64];
+	int i, num;
+	
+	num = va_arg(list, unsigned int);
+	i = 0;
+	while (num > 0)
+	{
+		b[i] = (num % 2) + 48;
+		num /= 2;
+		i++;
+	}
+	for (int j = i - 1; j >= 0; j--)
+	{
+		write(1, &(b[j]), 1);
+	}
 }
 
 
@@ -33,21 +34,21 @@ void print_binary(va_list list)
 */
 void print_octal(va_list list)
 {
-        int oct[100];
-        int i = 0;
-        unsigned int num;
+	int oct[100];
+	int i = 0;
+	unsigned int num;
         
-        num = va_arg(list, unsigned int);
-        while (num != 0)
-        {
-                oct[i]=(num % 8) + 48;
-                num = num / 8;
-                i++;
-        }
-        for (int x = i - 1; x >= 0; x--)
-        {
-               write (1,&oct[x],1);
-        }
+	num = va_arg(list, unsigned int);
+	while (num != 0)
+	{
+		oct[i]=(num % 8) + 48;
+		num = num / 8;
+		i++;
+	}
+	for (int x = i - 1; x >= 0; x--)
+	{
+		write (1,&oct[x],1);
+	}
 }
 
 
@@ -60,26 +61,26 @@ void print_octal(va_list list)
 */
 void print_hexa(va_list list ,int flag)
 {
-    char hex[100];
-    int unsigned i, remainder, num;
-
-    num = va_arg(list, unsigned int);
-    i = 0;
-    while (num != 0) {
-        remainder = num % 16;
-        if (remainder < 10)
-             hex[i] = remainder + 48;
-
-        else {
-                if (flag)
-                        hex[i] = remainder + 55;
-                else
-                        hex[i] = remainder + 87;
-        }
-        i++;
-        num /= 16;
+	char hex[100];
+	int unsigned i, remainder, num;
+    	num = va_arg(list, unsigned int);
+	i = 0;
+	while (num != 0) 
+	{
+		remainder = num % 16;
+		if (remainder < 10)
+		hex[i] = remainder + 48;
+	else 
+	{
+		if (flag)
+			hex[i] = remainder + 55;
+		else
+		hex[i] = remainder + 87;
+	}
+	i++;
+	num /= 16;
     }
     for (int j = i - 1; j >= 0; j--) {
-        write(1, &hex[j], 1);
+
+	    write(1, &hex[j], 1);
     }
-}
